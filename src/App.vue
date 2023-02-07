9aed5476c566a2f4b0d87eb6b16e6cdbc52eee9f
Aime <aimeagustinagarces@gmail.com>
2023-02-06 20:56:57 -0600

    add package and html

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<script setup>
+import { ref, onMounted, computed, watch } from 'vue'
+
+const todos = ref([])
+const name = ref('')
+
+const input_content = ref('')
+const input_category = ref(null)
+
+const todos_asc = computed(() => todos.value.sort((a, b) => {
+  return b.createdAt - a.createdAt
+}))
+
+const addTodo = () => {
+  if(input_content.value.trim() === '' || input_category.value === null){
+    return 
+  }
+  todos.value.push({
+    content: input_content.value
+  })
+}
+//set value in local storage
+watch(name, (newVal) => {
+  localStorage.setItem("name", newVal)
+})
+//save the value
+onMounted(() => {
+  name.value = localStorage.getItem('name') || ''
+})
+
+</script>
+
+<template>
+  <main class="app">
+
+    <section class="greeting">
+      <h2 class="title">
+        What's up, <input type="text" class="text" placeholder="Name here" 
+        v-model="name"/>
+      </h2>
+    </section>
+
+    <section class="create-todo">
+      <h3>Create a todo</h3>
+
+      <form @submit.prevent="addTodo">
+        <h4>What's on your todo</h4>
+        <input 
+          type="text" 
+          placeholder="e.g make a video"
+          v-model="input_content"/>
+          <h4>Pick a category</h4>
+          <div class="options">
+            <label for="">
+              <input 
+                type="radio" 
+                name="category" 
+                value="business"
+                v-model="input_category"/>
+                <span class=""></span>
+                <div>Business</div>
+            </label>
+
+            <label for="">
+              <input 
+                type="radio" 
+                name="category" 
+                value="personal"
+                v-model="input_category"/>
+                <span class=""></span>
+                <div>Personal</div>
+            </label>
+            <input type="submit" value="Add todo">
+          </div>
+      </form>
+    </section>
+  </main>
+</template>
