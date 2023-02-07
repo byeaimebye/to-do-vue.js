eed52b207b6687e1f411946851139354844e0e4d
Aime <aimeagustinagarces@gmail.com>
2023-02-06 21:28:10 -0600

    add hidratation to app

--- src/App.vue
+++ src/App.vue
@@ -16,9 +16,23 @@ const addTodo = () => {
     return 
   }
   todos.value.push({
-    content: input_content.value
+    content: input_content.value,
+    category: input_category.value,
+    done: false,
+    createdAt: new Date().getTime()
   })
+
+  input_content.value = ''
+  input_category.value = null
 }
+
+const removeTodo = todo => {
+  todos.value = todos.value.filter(t => t !== todo)
+}
+
+watch(todos, newVal => {
+  localStorage.setItem('todos', JSON.stringify(newVal))
+}, {deep:true} )
 //set value in local storage
 watch(name, (newVal) => {
   localStorage.setItem("name", newVal)
@@ -26,6 +40,7 @@ watch(name, (newVal) => {
 //save the value
 onMounted(() => {
   name.value = localStorage.getItem('name') || ''
+  todos.value = JSON.parse(localStorage.getItem('todos') || [])
 })
 
 </script>
@@ -73,6 +88,28 @@ onMounted(() => {
             <input type="submit" value="Add todo">
           </div>
       </form>
+    </section>
+
+    <section class="todo-list">
+      <h3>TODO LIST</h3>
+      <div class="list">
+        <div v-for="todo in todos_asc" :class="`todo-item ${todo.done && 'done'}`">
+        
+          <label>
+            <input type="checkbox" v-model="todo.done">
+            <span :class="`bubble ${todo.category}`"></span>
+          </label>
+
+          <div class="todo-content">
+            <input type="text" v-model="todo.content" />
+          </div>
+
+          <div class="actions">
+            <button class="delete" @click="removeTodo(todo)">Delete</button>
+          </div>
+          
+        </div>
+      </div>
     </section>
   </main>
 </template>
